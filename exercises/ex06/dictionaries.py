7f94888bf603a393129547be566a3c0066746b62
Madeline Vessey <mvessey@email.unc.edu>
2021-10-20 23:07:01 -0400

    Exercise 6 maybe finished

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,32 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730409403"
 
-# Define your functions below
+
+def invert(norm_dict: dict[str, str]) -> dict[str, str]:
+    inverted_dict = {}
+    for key, value in norm_dict.items():
+        if value in inverted_dict.keys():
+            raise KeyError("Two keys of same value cannot exist.")
+        else:
+            inverted_dict[value] = []
+            inverted_dict[value].append(key)
+    return inverted_dict
+
+
+def favorite_color(colors: dict[str, str]) -> str:
+    the_favorite_color = {}
+    for key, value in colors.items():
+        the_favorite_color.setdefault(value, set()).add(key)
+    favorite_color = [key for key, values in the_favorite_color.items() if len(values) > 1]
+    return str(favorite_color)
+
+
+def count(list_values: list[str]) -> dict[str, int]:
+    initial_dictionary = {}
+    for item in list_values:
+        if (item in initial_dictionary):
+            initial_dictionary[item] += 1
+        else:
+            initial_dictionary[item] = 1
+    return initial_dictionary
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,51 @@
 """Unit tests for dictionary functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
 
-__author__ = "123456789"
+from exercises.ex06.dictionaries import invert, favorite_color, count
+
+__author__ = "730409403"
+
+
+def test_invert_equal_value_key() -> None:
+    """Testing if one value = key with inverse function."""
+    assert invert({'a': 'a', 'b': 'b'}) == {'a': ['a'], 'b': ['b']}
+
+
+def test_invert_long() -> None:
+    """Testing if long dictionary correctly inverts."""
+    assert invert({'a': 'z', 'b': 'y', 'c': 'x'}) == {'z': ['a'], 'y': ['b'], 'x': ['c']}
+
+
+def test_invert_ending_value() -> None:
+    """Testing endpoints."""
+    assert invert({'Boron': 'Boron', 'Helium': 'Sodium'}) == {'Boron': ['Boron'], 'Sodium': ['Helium']}
+
+
+def test_favorite_color_equal() -> None:
+    """Favorite colors with one value occuring more."""
+    assert favorite_color({"Mark": "yellow", "Elena": "Blue", "Greg": "Blue"}) == "['Blue']"
+
+
+def test_favorite_color_one_value() -> None:
+    """Testing favorite color with all one value."""
+    assert favorite_color({"Mark": "Blue", "Elena": "Blue", "Greg": "Blue"}) == "['Blue']"
+
+
+def test_favorite_color() -> None:
+    """Testing favorite color with favorite color as the beginnig and end of the dictionary."""
+    assert favorite_color({"Mark": "Blue", "Elena": "Orange", "Patricia": "Pink", "Greg": "Blue"}) == "['Blue']"
+
+
+def test_count_all_occur_equally() -> None:
+    """Testing when all string items occur equally in list."""
+    assert count(["Bob", "Henry", "Andrew"]) == {'Bob': 1, 'Henry': 1, 'Andrew': 1}
+
+
+def test_count_middle_occurs_most() -> None:
+    """Testing count when middle of list has strings that occur the most."""
+    assert count(["Ken", "Barbie", "Barbie", "Midge"]) == {'Ken': 1, 'Barbie': 2, 'Midge': 1}
+
+
+def test_count_outer_items_greater() -> None:
+    """Testing count when greater amount of same strings occurs on the endpoints of the list."""
+    assert count(["Ken", "Barbie", "Midge", "Ken"]) == {'Barbie': 1, 'Midge': 1, 'Ken': 2}
